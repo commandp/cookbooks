6f9c42f36ebb0c81c3a0c2c67babf839a7e41444
Jimmy Kuo <jimmy@gogojimmy.net>
2014-10-02 18:22:24 +0800

    加上 set_user recipe 讓 worker layer 可以建立 deploy user

---
+++ commandp/recipes/set_user.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: commandp
+# Recipe:: configs
+#
+# Copyright 2014, Jimmy Kuo
+#
+# All rights reserved - Do Not Redistribute
+#
+
+opsworks_deploy_user do
+  deploy_data deploy
+end
