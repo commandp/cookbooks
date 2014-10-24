e7f05f376cec8334e356841e516ad80c8857bee2
Jimmy Kuo <jimmy@gogojimmy.net>
2014-10-24 21:09:05 +0800

    加上缺少的 application 變數

--- commandp/recipes/copy_doc.rb
+++ commandp/recipes/copy_doc.rb
@@ -1,4 +1,11 @@
-template "#{node[:nginx][:dir]}/sites-enabled/doc" do
-  source 'nginx_doc.erb'
+node[:deploy].each do |application, deploy|
+
+  template "#{node[:nginx][:dir]}/sites-enabled/doc" do
+    source 'nginx_doc.erb'
+    variables(
+      'application' => application
+    )
+  end
+
 end
 
