2aabae4a15f954d151c07cd847a0e62d14699cad
Sammy Lin <sammylintw@gmail.com>
2015-07-08 12:47:51 +0800

    複寫 aws nodejs recipe

---
+++ deploy/recipes/nodejs.rb
@@ -0,0 +1,47 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'nodejs'
+    Chef::Log.debug("Skipping deploy::nodejs for application #{application} as it is not a node.js app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  execute 'Build node js' do
+    user deploy[:user]
+    group deploy[:group]
+    cwd ::File.join(deploy[:deploy_to], 'current')
+    command '/usr/local/bin/npm run build'
+    not_if { ::File.directory?("#{deploy[:deploy_to]}/current/build") }
+  end
+
+  opsworks_nodejs do
+    deploy_data deploy
+    app application
+  end
+
+  application_environment_file do
+    user deploy[:user]
+    group deploy[:group]
+    path ::File.join(deploy[:deploy_to], "shared")
+    environment_variables deploy[:environment_variables]
+  end
+
+  ruby_block "restart node.js application #{application}" do
+    block do
+      Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
+      Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
+      $? == 0
+    end
+  end
+end
