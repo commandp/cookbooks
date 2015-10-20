784858abbc15af470b0834e110466b293dddec73
Sammy Lin <sammylintw@gmail.com>
2015-10-20 12:22:04 +0800

    fix cookbook

--- imagemagick-webp/recipes/default.rb
+++ imagemagick-webp/recipes/default.rb
@@ -21,15 +21,12 @@ end
 execute "Unzip ImageMagick #{node['imagemagick']['version']}" do
   command "tar xvJf #{Chef::Config[:file_cache_path]}/ImageMagick-#{node['imagemagick']['version']}.tar.xz"
   cwd "#{node['imagemagick']['install_path']}"
-  not_if File.directory?("#{Chef::Config[:file_cache_path]}/ImageMagick-#{node['imagemagick']['version']}")
-  not_if "which convert"
   not_if "convert -version | grep #{node['imagemagick']['version']}"
 end
 
 execute "Configure ImageMagick #{node['imagemagick']['version']}" do
   command "./configure && make && make install"
   cwd "#{node['imagemagick']['install_path']}/ImageMagick-#{node['imagemagick']['version']}"
-  not_if "which convert"
   not_if "convert -version | grep #{node['imagemagick']['version']}"
 end
 
