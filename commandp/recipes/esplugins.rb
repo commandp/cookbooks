2589b15b2a8a49753cfe0222379639b813ea348b
Rich Ke <run26kimo@gmail.com>
2016-01-29 19:06:46 +0800

    esplugin add install_plugin_es_marvel

--- commandp/recipes/esplugins.rb
+++ commandp/recipes/esplugins.rb
@@ -7,3 +7,13 @@ script "install_plugin_es_head" do
     EOH
   not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
+
+script "install_plugin_es_marvel" do
+  interpreter "bash"
+  user "root"
+  cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+  code <<-EOH
+    plugin -install elasticsearch/marvel/latest
+    EOH
+  not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/marvel") }
+end
