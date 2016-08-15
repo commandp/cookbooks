59d9b4acb3ed821e7c692c3f01134cb8bc95111c
Sammy Lin <sammylintw@gmail.com>
2016-08-15 14:25:52 +0800

    建立 REVISION 在 current 目錄底下方便查詢 commit 的 sha1 fix

--- deploy/recipes/nodejs.rb
+++ deploy/recipes/nodejs.rb
@@ -79,7 +79,7 @@ node[:deploy].each do |application, deploy|
   execute 'Get Short Git Version Hash Into REVISION ' do
     user deploy[:user]
     group deploy[:group]
-    path ::File.join(deploy[:deploy_to], "current")
+    cwd ::File.join(deploy[:deploy_to], "current")
     command "git rev-parse --short HEAD > REVISION"
   end
 
