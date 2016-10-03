e3a0789bcd542bb5ed5ad90ba76782c833f84da1
Jonny Lai <jonny.lai@commandp.com>
2016-10-03 19:10:41 +0800

    fix syntax error
    經 SammyLin 解說後，移除用不到的 code。

--- commandp/recipes/zabbix_monitoring_nginx.rb
+++ commandp/recipes/zabbix_monitoring_nginx.rb
@@ -1,43 +1,41 @@
-include_recipe 'zabbix-agent::service'
+# ============================================================
+#  Author: Jonny / jonny.lai (at) commandp.com
+#  Filename: zabbix_monitoring_nginx.rb
+#  Modified: 2016-10-03 19:05
+#  Description: Deploy nginx status module of Zabbix.
+#  Reference: 
+#
+#   1. Zabbix 监控 nginx 性能 (113) | 运维生存时间
+#     - http://www.ttlsa.com/zabbix/zabbix-monitor-nginx-performance/
+#
+# =========================================================== 
 
-node[:deploy].each do |application, deploy|
-  if node[:zabbix-agent]
+# Install necessary package.
+package 'Install wget' do
+  package_name 'wget'
+  action :install
+end
 
-    # Install necessary package.
-    package 'Install wget' do
-      package_name 'wget'
-      action :install
-    end
-    
-    # Create directory for 'nginx-check.sh'.
-    directory '/etc/zabbix/bin' do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      action :create
-    end
-    
-    # Put nginx-check.sh to `/etc/zabbix/bin/`.
-    cookbook_file '/etc/zabbix/bin/nginx-check.sh' do
-      source: 'zabbix/nginx-check.sh'
-      owner 'root'
-      group 'root'
-      mode 0755
-    end
-    
-    # Put nginx.conf to `/etc/zabbix/zabbix_agentd.conf.d`.
-    cookbook_file '/etc/zabbix/zabbix_agentd.conf.d/nginx.conf' do
-      source: 'zabbix/zabbix_agentd_nginx.conf'
-      owner 'root'
-      group 'root'
-      mode 0644
-    end
+# Create directory for 'nginx-check.sh'.
+directory '/etc/zabbix/bin' do
+  mode '0755'
+  action :create
+end
 
-    # restart service.
-    service "zabbix-agent" do
-      supports :restart => true, :reload => true, :status => true
-      action :reload
-    end
+# Put nginx-check.sh to `/etc/zabbix/bin/`.
+cookbook_file '/etc/zabbix/bin/nginx-check.sh' do
+  source 'zabbix/nginx-check.sh'
+  mode 0755
+end
 
-  end
+# Put nginx.conf to `/etc/zabbix/zabbix_agentd.conf.d`.
+cookbook_file '/etc/zabbix/zabbix_agentd.conf.d/nginx.conf' do
+  source 'zabbix/zabbix_agentd_nginx.conf'
+  mode 0644
+end
+
+# restart service.
+service "zabbix-agent" do
+  supports :restart => true, :reload => true, :status => true
+  action :reload
 end
