bee68343d7f337d256129a643737a13b8ca3b4a6
Jonny Lai <jonny.lai@commandp.com>
2016-10-27 17:38:52 +0800

    add dependency for zabbix-agent recipe

--- commandp/metadata.rb
+++ commandp/metadata.rb
@@ -5,3 +5,4 @@ description      "Configures commandp app server environment."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "aws"
+depends          "zabbix-agent", ">= 0.14.2"
